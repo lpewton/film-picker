12632ecf0d8062acf6c777930e425bc237c4c40a
lpewton <liampewton99@gmail.com>
2023-01-07 02:14:44 +0000

    Add show_films function, to show a list of the films and their properties

--- run.py
+++ run.py
@@ -13,5 +13,20 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("film_picker")
 
 films = SHEET.worksheet("films")
-data = films.get_all_values()
-print(data)
+
+def show_films():
+    """
+    Show a list of the films with their respectives categories, genre, synopsis and rating
+    """
+    all_films = films.get_all_values()
+    film_num = 0
+    for ind in range(len(all_films)):
+        film = all_films[ind + 1]
+        title = film[0]    
+        genre = film[1]
+        descr = film[2]
+        rating = film[3]
+        film_num = film_num + 1    
+        print(f"{film_num}: {title}. Genre: {genre}. Synopsis: {descr} Rating: {rating}\n")   
+
+show_films()
