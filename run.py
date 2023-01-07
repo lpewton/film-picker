ddf8516fe873df218f69fe31e09883a84b48408d
lpewton <liampewton99@gmail.com>
2023-01-07 02:54:16 +0000

    Add pick_option function so user can interact with the program

--- run.py
+++ run.py
@@ -16,31 +16,52 @@ films = SHEET.worksheet("films")
 
 def set_up():
     """
-    Sets introduction up snd gives you the option to see a list of the films, 
+    Sets introduction up snd gives you the option to see a list of the films,
     choose a random film based on a category or add a film to the list.
     """
     print("Welcome to Film Picker!")
     print("Please choose an action:\n")
     print("1: See all available films")
     print("2: Get a random film suggestion based on a category")
-    print("3: Add a new film to the system")
+    print("3: Add a new film to the system\n")
     pick_option()
 
+def pick_option():
+    options = input("Introduce option: ")
+    try:
+        if int(options) == 1:
+            show_films()
+        elif int(options) == 2:
+            print("2")
+        elif int(options) == 3:
+            print("3")
+        else:
+            print("Please introduce one of the options")
+            pick_option()
+    except:
+        print("Please introduce one of the options")
+        pick_option()
+
+    
 
 def show_films():
     """
-    Show a list of the films with their respectives categories, genre, synopsis and rating
+    Show a list of the films with their respectives categories, genre,
+    synopsis and rating
     """
-    all_films = films.get_all_values()
+    all_films = films.get_all_values()[1:]
     film_num = 0
     for ind in range(len(all_films)):
-        film = all_films[ind + 1]
-        title = film[0]    
+        film = all_films[ind]
+        title = film[0]
         genre = film[1]
         descr = film[2]
         rating = film[3]
-        film_num = film_num + 1    
-        print(f"{film_num}: {title}. Genre: {genre}. Synopsis: {descr} Rating: {rating}\n")   
+        film_num = film_num + 1
+        print(f"\n{film_num}: {title}")
+        print(f"Genre: {genre}")
+        print(f"Synopsis: {descr}")
+        print(f"Rating: {rating}\n")
 
 def main():
     set_up()
