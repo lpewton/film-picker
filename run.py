3095cb41a472729d49a283facad8eb988a578849
lpewton <liampewton99@gmail.com>
2023-01-08 03:03:04 +0000

    Add pick_random_category and pick_random film for program to generate a film suggestion based on user's category choice

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import random
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -16,33 +17,35 @@ films = SHEET.worksheet("films")
 
 def set_up():
     """
-    Sets introduction up snd gives you the option to see a list of the films,
-    choose a random film based on a category or add a film to the list.
+    Sets introduction up.
     """
     print("Welcome to Film Picker!")
-    print("Please choose an action:\n")
+    print("Please choose an option:\n")
     print("1: See all available films")
     print("2: Get a random film suggestion based on a category")
     print("3: Add a new film to the system\n")
     pick_option()
 
 def pick_option():
+    """ 
+    Give user options to see a list of the films, choose a random 
+    film based on a category or add a film to the list.
+    """
+
     options = input("Introduce option: ")
     try:
         if int(options) == 1:
             show_films()
         elif int(options) == 2:
-            print("2")
+            pick_random_category()
         elif int(options) == 3:
             print("3")
         else:
             print("Please introduce one of the options")
             pick_option()
-    except:
+    except ValueError:
         print("Please introduce one of the options")
         pick_option()
-
-    
 
 def show_films():
     """
@@ -62,6 +65,62 @@ def show_films():
         print(f"Genre: {genre}")
         print(f"Synopsis: {descr}")
         print(f"Rating: {rating}\n")
+
+def pick_random_category():
+    """
+    Allows user to pick a category for film to watch
+    """
+    print("Pick a Category:")
+    print("1: Comedy")
+    print("2: Drama")
+    print("3: Horror")
+    print("4: History")
+    print("5: Action")
+    print("6: Crime")
+    print("7: Romance")
+    print("8: Fantasy")
+    print("9: Mystery\n")
+    categories = int(input("Introduce category: "))
+    if categories == 1:
+        category = "Comedy"
+    if categories == 2:
+        category = "Drama"
+    if categories == 3:
+        category = "Horror"
+    if categories == 4:
+        category = "History"
+    if categories == 5:
+        category = "Action"
+    if categories == 6:
+        category = "Crime"
+    if categories == 7:
+        category = "Romance"
+    if categories == 8:
+        category = "Fantasy"
+    if categories == 9:
+        category = "Mistery"
+
+    pick_random_film(category)
+    
+def pick_random_film(category):
+    """
+    Picks a random film from the category the user picked
+    """
+    all_films = films.get_all_values()[1:]
+    category_list = []
+    for ind in range(len(all_films)):
+        if category in all_films[ind]:
+            category_list.append(all_films[ind])
+    random_num = random.randint(0, len(category_list))
+    film = category_list[random_num]
+    title = film[0]
+    genre = film[1]
+    descr = film[2]
+    rating = film[3]
+    print(f"\nTitle: {title}")
+    print(f"Genre: {genre}")
+    print(f"Synopsis: {descr}")
+    print(f"Rating: {rating}\n")
 
 def main():
     set_up()
